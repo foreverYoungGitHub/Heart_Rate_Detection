4416c4297ea41717319cee1a63238648001b2e29
刘阳 <Young@liuyangdeMacBook-Pro.local>
2017-05-18 10:01:04 -0400

    slightly change the functions

--- ColorMagnify.cpp
+++ ColorMagnify.cpp
@@ -1,4 +1,16 @@
-//Created by Ying on 06/15/2017
+/**
+ * Questions:
+ * 	1. I didn't see the color transformation for the original image, which I think it may be necessary.
+ * 	2. I not familiar with the space filter, so I can not confirm about whether the Gaussian pyramid is correct,
+ * 		do we need to keep the image as the same size.
+ * 	3. Could we directly use the amplified result from space and time filter?
+ * 	4. Let us specify the mathematics in this afternoon.
+ *
+ * Others:
+ * 	1. I think we could transform the RGB color space to the YUV color space rather than the YIQ color space;
+ * 	2. In the near future, we should also add the other two amplify function (motion and phase).
+ *
+ */
 
 #include "ColorMagnify.h"
 
@@ -11,13 +23,36 @@ ColorMagnify::~ColorMagnify() {}
 // main process of color magnification
 void ColorMagnify::EVM(std::vector<cv::Mat>&frames, std::vector<cv::Mat>& imgs)
 {
-	for (int i=0; i < frames.size(); i++) {
-		buildGaussianPyramid(frames[i], levels_, pyramid);
-		downSampledFrames.push_back(pyramid[levels_ - 1]);
+
+	// down sampled frames - space filter
+	std::vector<cv::Mat> downSampledFrames;
+
+	for(auto frame : frames) {
+		auto pyramid = buildGaussianPyramid(frame, levels_);
+		downSampledFrames.push_back(pyramid);
 	}
-	concat(downSampledFrames, videoMat);
+
+//	for (int i=0; i < frames.size(); i++) {
+//		// Gaussian pyramid
+//		std::vector<cv::Mat> pyramid;
+//		buildGaussianPyramid(frames[i], levels_, pyramid);
+//		downSampledFrames.push_back(pyramid[levels_ - 1]);
+//	}
+
+	// concatenated image of all the down sampled frames
+	auto videoMat = concat(downSampledFrames);
+//	concat(downSampledFrames, videoMat);
+
+	// filtered image after concatenate
+	cv::Mat filtered;
 	temporalIdealFilter(videoMat, filtered);
-	amplify(filtered, amplified);
+
+	// amplified image
+	auto amplified = amplify(filtered);
+
+
+	// frames after temporal filtering
+	std::vector<cv::Mat > filteredFrames;
 	deConcat(amplified, downSampledFrames[0].size(), filteredFrames);
 
 	int n = filteredFrames.size();
@@ -25,21 +60,30 @@ void ColorMagnify::EVM(std::vector<cv::Mat>&frames, std::vector<cv::Mat>& imgs)
 
 	for (int j=0; j < frames.size(); j++) {
 
+		// motion image
+		cv::Mat motion;
+
 		upsamplingFromGaussianPyramid(filteredFrames[j], levels_, motion);
 		resize(motion, motion, frames[j].size());
-		temp = frames[j] + motion;
+
+		// temp image
+		cv::Mat temp = frames[j] + motion;
 		double minVal, maxVal;
 		minMaxLoc(temp, &minVal, &maxVal);//find minimun and maximum intensities
+
+		// output frame
+		cv::Mat output;
 		temp.convertTo(output, CV_8UC3, 255.0 / (maxVal - minVal),
 			-minVal * 255.0 / (maxVal - minVal));
 		imgs.push_back(output);
 	}
 }
+
 /**
 * buildGaussianPyramid	-	construct a gaussian pyramid from a given image
 *
 * @param img		-	source image
-* @param levels	-	levels of the destinate pyramids
+* @param levels		-	levels of the destinate pyramids
 * @param pyramid	-	destinate image
 *
 * @return true if success
@@ -57,6 +101,26 @@ void ColorMagnify::buildGaussianPyramid(const cv::Mat &img, const int levels,
 		cv::pyrDown(currentImg, down);
 		pyramid.push_back(down);
 		currentImg = down;
+	}
+}
+
+/**
+* buildGaussianPyramid	-	construct a gaussian pyramid from a given image
+*
+* @param img		-	source image
+* @param levels		-	levels of the destinate pyramids
+*
+* @return the destinate image
+*/
+cv::Mat buildGaussianPyramid(const cv::Mat &img, const int levels) {
+	if (levels < 1) {
+		perror("Levels should be larger than 1");
+	}
+	auto currentImg = img;
+	for (int l = 0; l<levels; l++) {
+		cv::Mat down;
+		cv::pyrDown(currentImg, down);
+		currentImg = std::move(down);
 	}
 }
 
@@ -154,6 +218,15 @@ void ColorMagnify::amplify(const cv::Mat &src, cv::Mat &dst)
 }
 
 /**
+* amplify			-	ampilfy the motion
+*
+* @param filtered	- motion image
+*/
+cv::Mat ColorMagnify::amplify(const cv::Mat &src) {
+	return src * alpha_;
+}
+
+/**
 * upsamplingFromGaussianPyramid	-	up-sampling an image from gaussian pyramid
 *
 * @param src		-	source image
@@ -193,6 +266,32 @@ void ColorMagnify::concat(const std::vector<cv::Mat> &frames, cv::Mat &dst)
 		reshaped.copyTo(line);
 	}
 	temp.copyTo(dst);
+}
+
+/**
+* concat		-	concat all the frames into a single large Mat
+*              where each column is a reshaped single frame
+*
+* @param frames	-	frames of the video sequence
+*
+* @return 		-	destinate concatnate image
+*/
+cv::Mat ColorMagnify::concat(const std::vector<cv::Mat> &frames) {
+	cv::Size frameSize = frames[0].size();
+	int length = frames.size();
+
+	cv::Mat temp(frameSize.width*frameSize.height, length, CV_32FC3);
+	for (int i = 0; i < length; i++) {
+		// get a frame if any
+		auto input = frames[i];
+		// reshape the frame into one column
+		cv::Mat reshaped = input.reshape(3, input.cols*input.rows).clone();
+		cv::Mat line = temp.col(i);
+		// save the reshaped frame to one column of the destinate big image
+		reshaped.copyTo(line);
+	}
+
+	return temp;
 }
 /**
 * deConcat	-	de-concat the concatnate image into frames
--- ColorMagnify.h
+++ ColorMagnify.h
@@ -1,4 +1,4 @@
-//Created by Ying on 06/15/2017
+//
 
 #ifndef COLORMODIFY_H
 #define COLORMODIFY_H
@@ -15,9 +15,15 @@ public:
 
 	//main process
 	void EVM(std::vector<cv::Mat>&frames, std::vector<cv::Mat>& imgs);
+
 	// build a gaussian pyramid
 	void buildGaussianPyramid(const cv::Mat &img, const int levels,
 		std::vector<cv::Mat> &pyramid);
+	cv::Mat buildGaussianPyramid(const cv::Mat &img, const int levels);
+
+	// concat images into a large Mat
+	void concat(const std::vector<cv::Mat> &frames, cv::Mat &dst);
+	cv::Mat concat(const std::vector<cv::Mat> &frames);
 
 	// temporal ideal bandpass filtering
 	void temporalIdealFilter(const cv::Mat &src,
@@ -28,35 +34,18 @@ public:
 
 	// amplify motion
 	void amplify(const cv::Mat &src, cv::Mat &dst);
+	cv::Mat amplify(const cv::Mat &src);
 
 	// up-sampling an image from gaussian pyramid
 	void upsamplingFromGaussianPyramid(const cv::Mat &src, const int levels,
 		cv::Mat &dst);
 
-	// concat images into a large Mat
-	void concat(const std::vector<cv::Mat> &frames, cv::Mat &dst);
+
 
 	// de-concat the concatnate image into frames
 	void deConcat(const cv::Mat &src, const cv::Size &frameSize, std::vector<cv::Mat> &frames);
 
-	// Gaussian pyramid
-	std::vector<cv::Mat> pyramid;
-	// down sampled frames
-	std::vector<cv::Mat> downSampledFrames;
-	// concatenated image of all the down sampled frames
-	cv::Mat videoMat;
-	// filtered image after concatenate
-	cv::Mat filtered;
-	// amplified image
-	cv::Mat amplified;
-	// frames after temporal filtering
-	std::vector<cv::Mat > filteredFrames;
-	// motion image
-	cv::Mat motion;
-	// temp image
-	cv::Mat temp;
-	// output frame
-	cv::Mat output;
+
 	//
 	double fl_;
 	double fh_;
